12977c7ed2d278c7b5b291c2309a4b9705c1e47b
FominNickolai <giraffe.in.ua@gmail.com>
2017-02-23 00:06:24 +0200

    Initial Commit

---
+++ Medicine/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Medicine
+//
+//  Created by Fomin Nickolai on 2/23/17.
+//  Copyright © 2017 Fomin Nickolai. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
